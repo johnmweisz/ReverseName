fd543c5d9bb73c19138c80f2ee6a81f329211718
John Weisz <johnmweisz@gmail.com>
2019-12-11 11:33:55 -0800

    Add project files.

---
+++ ReverseName/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a name to reverse");
+            var input = Console.ReadLine();
+            if (!String.IsNullOrWhiteSpace(input))
+            {
+                var stringArr = new char[input.Length];
+                for (int i = 0; i < stringArr.Length; i++)
+                {
+                    stringArr[i] = input[i];
+                }
+
+                var reversedArr = stringArr.Reverse();
+                var returnString = String.Join("", reversedArr);
+                Console.WriteLine(returnString);
+            }
+        }
+    }
+}
